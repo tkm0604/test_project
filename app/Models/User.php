ef90a0e68172db4a9d29275fa1844943647204f3
yamaguchi.takumi <gu.jp06042gmail.com>
2024-11-07 17:29:22 +0900

    メール認証実装

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
+use App\Notifications\NewVerifyEmail;
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -56,5 +56,10 @@ class User extends Authenticatable
 
     public function roles(){
         return $this->belongsToMany(Role::class);
+    }
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new NewVerifyEmail());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,42 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard',[PostController::class,'index'])
-->middleware(['auth','verified'])
-->name('dashboard');
-
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::middleware(['auth','can:admin'])->group(function(){
-    Route::get('profile/index',[ProfileController::class,'index'])->name('profile.index');
-});
-
-Route::get('post/mypost',[PostController::class,'mypost'])->name('post.mypost');
-Route::get('post/mycomment',[PostController::class,'mycomment'])->name('post.mycomment');
-Route::resource('post', PostController::class);
-Route::post('post/comment/store',[CommentController::class, 'store'])->name('comment.store');
-
 //お問い合わせ
 Route::get('contact/create',[ContactController::class, 'create'])->name('contact.create');
 Route::post('contact/store',[ContactController::class, 'store'])->name('contact.store');
+
+// ログイン後の通常のユーザー画面
+Route::middleware(['verified'])->group(function(){
+    Route::get('/dashboard',[PostController::class,'index'])
+    ->middleware(['auth','verified'])
+    ->name('dashboard');
+    Route::get('post/mypost',[PostController::class,'mypost'])->name('post.mypost');
+    Route::get('post/mycomment',[PostController::class,'mycomment'])->name('post.mycomment');
+    Route::resource('post', PostController::class);
+    Route::post('post/comment/store',[CommentController::class, 'store'])->name('comment.store');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // 管理者用画面
+    Route::middleware(['auth','can:admin'])->group(function(){
+        Route::get('profile/index',[ProfileController::class,'index'])->name('profile.index');
+    });
+});
+
+
+
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// Route::middleware(['auth','can:admin'])->group(function(){
+//     Route::get('profile/index',[ProfileController::class,'index'])->name('profile.index');
+// });
+
+
+
+
 require __DIR__.'/auth.php';
